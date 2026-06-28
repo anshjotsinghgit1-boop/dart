641615a62bc6c59ac6c56d3366642034637843d1
anshjotsinghgit1-boop <anshjotsinghgit1@gmail.com>
2026-06-28 16:19:46 +0530

    Create firebase_options.dart

---
+++ lib/firebase_options.dart
@@ -0,0 +1,21 @@
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart' show defaultTargetPlatform, TargetPlatform;
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return android;
+      default:
+        return android;
+    }
+  }
+
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'AIzaSyBv6G1RLyjzwaRlKoQC0arys7MkXUWCFuY',
+    appId: '1:396369034947:android:bc1ce0adc56947232aec76',
+    messagingSenderId: '396369034947',
+    projectId: 'replyai-749f7',
+    storageBucket: 'replyai-749f7.firebasestorage.app',
+  );
+}
