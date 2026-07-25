c7c24d5773c03c678c8f99f88a15ce79b3ede0f9
anshjotsinghgit1-boop <anshjotsinghgit1@gmail.com>
2026-07-25 08:05:46 +0530

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.dart_app"
+    namespace = "com.prothon.rizzguru"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
