0f8858627d3abdbf766b95a87715ba42a708630f
anshjotsinghgit1-boop <anshjotsinghgit1@gmail.com>
2026-07-25 18:01:56 +0530

    Update login_screen.dart

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-
+import '../services/coins_service.dart';
 import 'home_screen.dart';
 
 class LoginScreen extends StatefulWidget {
