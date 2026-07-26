18e241936bb4c7e2f8ca2da64e58e6a15b426753
anshjotsinghgit1-boop <anshjotsinghgit1@gmail.com>
2026-07-26 09:24:50 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'firebase_options.dart';
-import 'screens/auth_gate.dart';
+import 'auth_gate.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
